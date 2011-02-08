5ab0424c984ca16165ad339096a11b2c60a6eb81
Anders Berggren <anders@halon.se>
2011-02-08 13:12:13 +0100

    Quite functional UI consoleo

--- consoleui/ui.sh
+++ consoleui/ui.sh
@@ -1,20 +1,55 @@
 #!/bin/bash
 
+# SUDO ITSELF
+if [ "$1" = "" ]
+then
+	sudo $0 noloop
+	exit
+fi
+
+ui_printerr()
+{
+	if [ "$?" != "0" ]
+	then
+		dialog --msgbox "`cat /tmp/log.dialog`" 20 70
+	fi
+}
+
 # DISK FUNCTIONS
 disk_rw()
 {
-	fsck -p / &>> /tmp/log.dialog
-	mount -o remount,rw / &>> /tmp/log.dialog
+	echo "Mounting R/W..." | dialog --progressbox 3 30
+	fsck -p / &> /tmp/log.dialog
+	ui_printerr
+	mount -o remount,rw / &> /tmp/log.dialog
+	ui_printerr
 }
 
 disk_ro()
 {
-	mount -o remount,ro / &>> /tmp/log.dialog
+	echo "Mounting R/O..." | dialog --progressbox 3 30
+	mount -o remount,ro / &> /tmp/log.dialog
+	ui_printerr
 }
 
 # NETWORK FUNCTIONS
+netconf_restart()
+{
+	ifdown $1 &> /tmp/log.dialog
+	ui_printerr
+	ifup $1 &> /tmp/log.dialog
+	ui_printerr
+}
+
+netconf_restart_quiet()
+{
+	ifdown $1 &> /dev/null 
+	ifup $1 &> /dev/null
+}
+
 netconf_remove()
 {
+	echo "Removing $1..." | dialog --progressbox 3 30
 	cp /etc/network/interfaces /tmp/net
 	# Convert file to one-liners only
 	sed -n '1h;1!H;${;g;s/\n\t/SLANGTAB/g;p;}' /tmp/net > /tmp/net2
@@ -30,6 +65,7 @@ netconf_static()
 {
 	disk_rw
 	netconf_remove $if
+	echo "Adding $if..." | dialog --progressbox 3 30
 	echo "auto $if" >> /etc/network/interfaces
 	ip=`sed -n "1p" /tmp/ui.dialog`
 	if [ "$ip" != "" ]
@@ -38,7 +74,10 @@ netconf_static()
 		echo -e "\taddress $ip" >> /etc/network/interfaces
 		mask=`sed -n "2p" /tmp/ui.dialog`
 		echo -e "\tnetmask $mask" >> /etc/network/interfaces
-		gw=`sed -n "3p" /tmp/ui.dialog`
+	fi
+	gw=`sed -n "3p" /tmp/ui.dialog`
+	if [ "$gw" != "" ]
+	then
 		echo -e "\tgateway $gw" >> /etc/network/interfaces
 	fi
 	ip=`sed -n "4p" /tmp/ui.dialog`
@@ -48,103 +87,90 @@ netconf_static()
 		echo -e "\taddress $ip" >> /etc/network/interfaces
 		mask=`sed -n "5p" /tmp/ui.dialog`
 		echo -e "\tnetmask $mask" >> /etc/network/interfaces
-		gw=`sed -n "6p" /tmp/ui.dialog`
+	fi
+	gw=`sed -n "6p" /tmp/ui.dialog`
+	if [ "$gw" != "" ]
+	then
 		echo -e "\tgateway $gw" >> /etc/network/interfaces
 	fi
 	disk_ro
-	show="no"
-	ifdown $if &>> /tmp/log.dialog
-	if [ "$?" != "0" ]
-	then
-		show="yes"
-		exit
-	fi
-	ifup $if &>> /tmp/log.dialog
-	if [ "$?" != "0" ]
-	then
-		show="yes"
-		exit
-	fi
-	if [ "$show" = "yes" ]
-	then
-		echo "----------------------" >> /tmp/log.dialog
-		date >> /tmp/log.dialog
-		echo "Setup of $1 failed." >> /tmp/log.dialog
-		echo "----------------------" >> /tmp/log.dialog
-		dialog --tailbox /tmp/log.dialog 20 75
-	fi
+	echo "Restarting $if..." | dialog --progressbox 3 30
+	netconf_restart $if
 }
 
-bring_all_if_up()
+netconf_dhcp()
 {
-	get_if_list
+	disk_rw
+	netconf_remove $if
+	echo "auto $if" >> /etc/network/interfaces
+	echo "iface $if inet dhcp" >> /etc/network/interfaces
+	# Run DHCP client, allow it to write /etc/resolv.conf
+	ifdown $if &> /tmp/log.dialog
+	ui_printerr
+	ifup $if &> /tmp/log.dialog
+	dialog --msgbox "`cat /tmp/log.dialog`" 20 70
+	# Kill the DHCP client in order to mount read-only
+	pkill -9 dhclient
+	disk_ro
+	# Now that we are read-only, we can restart dhclient
+	netconf_restart_quiet $if
+}
+
+netconf_dns()
+{
+	echo "Saving..." | dialog --progressbox 3 30
+	grep -v "^search " /etc/resolv.conf > /tmp/net
+	grep -v "^nameserver " /tmp/net > /tmp/net2
+	search=`sed -n "1p" /tmp/ui.dialog`
+	if [ "$search" != "" ]
+	then
+		echo "search $search" >> /tmp/net2
+	fi
+	for dns in `tail -n +2 /tmp/ui.dialog`
+	do
+		echo "nameserver $dns" >> /tmp/net2
+	done
+	disk_rw
+	cp /tmp/net2 /etc/resolv.conf
+	disk_ro
+}
+
+netconf_bring_all_if_up()
+{
+	netconf_get_if_list
 	for i in $if_list
 	do
 		ifconfig $i up 2>&1 > /dev/null
 	done
 }
 
-get_if_stat()
+netconf_get_if_stat()
 {
-	tmp=`mii-tool $1 |grep "link ok"`
+	tmp=`mii-tool $1 2> /dev/null |grep "link ok"`
 	if [ "$?" = "0" ]
 	then
-		if_stat=`mii-tool eth0|cut -d ' ' -f3`
+		if_stat=`mii-tool eth0 2> /dev/null | cut -d ' ' -f3`
 	else
 		if_stat="no-link"
 	fi
 }
 
-get_if_list()
+netconf_get_if_list()
 {
 	if_list=`ip -o link | cut -d':' -f2|tr -d ' '`
 }
-
-# FORK FUNCTIONS, THAT IS RUN IN THE BACKGROUND FOR THE SAKE OF LOG
-fork_dhcp()
-{
-	echo "----------------------" >> /tmp/log.dialog
-	date >> /tmp/log.dialog
-	echo "Starting DHCP on $1." >> /tmp/log.dialog
-	echo "One one port can use DHCP." >> /tmp/log.dialog
-	echo "Press EXIT when ready." >> /tmp/log.dialog
-	echo "----------------------" >> /tmp/log.dialog
-	pkill -9 dhclient
-	disk_rw
-	# Write configration 
-	netconf_remove $1
-	echo "auto $1" >> /etc/network/interfaces
-	echo "iface $1 inet dhcp" >> /etc/network/interfaces
-	# Sleep, in order for previous messages to be shown
-	sleep 3
-	# Run DHCP client, allow it to write /etc/resolv.conf
-	ifdown $1 &>> /tmp/log.dialog
-	ifup $1 &>> /tmp/log.dialog
-	# Kill the DHCP client in order to mount read-only
-	pkill -9 dhclient
-	disk_ro
-	echo "Complete." >> /tmp/log.dialog
-	# Now that we are read-only, we can restart dhclient
-	ifdown $1 &>> /dev/null 
-	ifup $1 &>> /dev/null
-}
-if [ "$1" = "dhcp" ]
-then
-	fork_dhcp $2 
-	exit
-fi
 
 # MENU  
 menu_network()
 {
 	while true
 	do
-		bring_all_if_up
-		get_if_list # sets if_list
+		netconf_bring_all_if_up # in order to see link status
+		netconf_get_if_list # sets if_list
 		if_str=""
 		for i in $if_list
 		do
-			get_if_stat $i # sets if_stat
+			netconf_get_if_stat $i # sets if_stat
 			if_str="$if_str $i $if_stat"
 		done
 		dialog --title "SLA-NG Network Settings" --menu \
@@ -180,29 +206,100 @@ menu_network_if()
 		fi
 		if [ "`cat /tmp/ui.dialog`" = "d" ]	
 		then
-			# Fork DHCP settings
-			$0 dhcp $if & 
-			dialog --tailbox /tmp/log.dialog 20 75
+			netconf_dhcp
 		fi
 		if [ "`cat /tmp/ui.dialog`" = "s" ]	
 		then
-			dialog --form "Type IP" 20 75 20 \
-			"IP Address"   1  2 "172.16.0.100"     1 20 40 40 \
-			"IP Netmask"   2  2 "255.255.255.0"    2 20 40 40 \
-			"IP Gateway"   3  2 "172.16.0.1"       3 20 40 40 \
-			"IPv6 Address" 5  2 "::1"              5 20 40 40 \
-			"IPv6 Bitmask" 6  2 "128"              6 20 40 40 \
-			"IPv6 Gateway" 7  2 "::1"              7 20 40 40 \
-			2> /tmp/ui.dialog
-			if [ "$?" = "1" ]
-			then
-				continue
-			fi
-			netconf_static
+			menu_network_if_static
 		fi
 	done
 }
 
+menu_network_if_static()
+{
+	# Extract IPv4 info 
+	cp /etc/network/interfaces /tmp/net
+	# Convert file to one-liners only
+	sed -n '1h;1!H;${;g;s/\n\t/SLANGTAB/g;p;}' /tmp/net > /tmp/net2
+	# Get right config line 
+	grep "iface $if inet " /tmp/net2 > /tmp/net
+	# Write back config, multi-line
+	sed 's/SLANGTAB/\n\t/g' /tmp/net > /tmp/net2
+	ip=`grep "address " /tmp/net2 | cut -d' ' -f2`
+	mask=`grep "netmask " /tmp/net2 | cut -d' ' -f2`
+	gw=`grep "gateway " /tmp/net2 | cut -d' ' -f2`
+	# Extract IPv6 info 
+	cp /etc/network/interfaces /tmp/net
+	# Convert file to one-liners only
+	sed -n '1h;1!H;${;g;s/\n\t/SLANGTAB/g;p;}' /tmp/net > /tmp/net2
+	# Get right config line 
+	grep "iface $if inet6 " /tmp/net2 > /tmp/net
+	# Write back config, multi-line
+	sed 's/SLANGTAB/\n\t/g' /tmp/net > /tmp/net2
+	ip6=`grep "address " /tmp/net2 | cut -d' ' -f2`
+	mask6=`grep "netmask " /tmp/net2 | cut -d' ' -f2`
+	gw6=`grep "gateway " /tmp/net2 | cut -d' ' -f2`
+	dialog --form "Type the addresses. Leave fields empty to omit them." \
+		14 70 7 \
+		"IP Address"   1  2 "$ip"     1 20 43 100 \
+		"IP Netmask"   2  2 "$mask"   2 20 43 100 \
+		"IP Gateway"   3  2 "$gw"     3 20 43 100 \
+		"IPv6 Address" 5  2 "$ip6"    5 20 43 100 \
+		"IPv6 Bitmask" 6  2 "$mask6"  6 20 43 100 \
+		"IPv6 Gateway" 7  2 "$gw6"    7 20 43 100 \
+		2> /tmp/ui.dialog
+	if [ "$?" = "0" ]
+	then
+		netconf_static
+	fi
+}
+
+menu_dns()
+{
+	s=`grep search /etc/resolv.conf | sed -n "1p"`
+	s=`echo $s | sed "s/search //"`
+	d1=`grep nameserver /etc/resolv.conf | sed -n "1p"`
+	d1=`echo $d1 | sed "s/nameserver //"`
+	d2=`grep nameserver /etc/resolv.conf | sed -n "2p"`
+	d2=`echo $d2 | sed "s/nameserver //"`
+	d3=`grep nameserver /etc/resolv.conf | sed -n "3p"`
+	d3=`echo $d3 | sed "s/nameserver //"`
+	dialog --form "Type the name servers to be used." 11 71 4 \
+	"Search domain" 1 2 "$s"  1 20 40 140 \
+	"Nameserver #1" 2 2 "$d1" 2 20 40 140 \
+	"Nameserver #2" 3 2 "$d2" 3 20 40 140 \
+	"Nameserver #3" 4 2 "$d3" 4 20 40 140 \
+	2> /tmp/ui.dialog
+	if [ "$?" = "0" ]
+	then
+		netconf_dns
+	fi
+}
+
+menu_ping()
+{
+	dialog --inputbox "Type an address to ping" \
+	7 40 www.tele2.se 2> /tmp/ui.dialog
+	ping -c 10 `cat /tmp/ui.dialog` &> /tmp/ui.dialog &
+	dialog --tailbox /tmp/ui.dialog 20 75
+	pkill -9 ping
+}
+
+menu_log()
+{
+	dialog --tailbox /var/log/messages 20 75
+}
+
+menu_shell()
+{
+	clear # clear screen
+	echo "Starting bash..."
+	reset # reset terminal; dialog may fuck it up :)
+	echo "Press CTRL+d to return to the console."
+	bash
+}
+
+# MAIN MENU LOOP
 while true
 do
 	dialog  --title "SLA-NG Configuration Console" \
@@ -211,6 +308,7 @@ do
 		n "Network Interface Settings" \
 		d "DNS Settings" \
 		p "Ping Host" \
+		l "SLA-NG Daemon Log" \
 		s "Start Shell (bash)"  \
 		2> /tmp/ui.dialog
 	if [ "$?" -eq 1 ]
@@ -223,54 +321,19 @@ do
 	fi
 	if [ "`cat /tmp/ui.dialog`" = "d" ]	
 	then
-		s=`grep search /etc/resolv.conf | sed -n "1p"`
-		s=`echo $s | sed "s/search //"`
-		d1=`grep nameserver /etc/resolv.conf | sed -n "1p"`
-		d1=`echo $d1 | sed "s/nameserver //"`
-		d2=`grep nameserver /etc/resolv.conf | sed -n "2p"`
-		d2=`echo $d2 | sed "s/nameserver //"`
-		d3=`grep nameserver /etc/resolv.conf | sed -n "3p"`
-		d3=`echo $d3 | sed "s/nameserver //"`
-		dialog --form "Type the name servers to be used." 11 71 4 \
-		"Search domain" 1 2 "$s"  1 20 40 40 \
-		"Nameserver #1" 2 2 "$d1" 2 20 40 40 \
-		"Nameserver #2" 3 2 "$d2" 3 20 40 40 \
-		"Nameserver #3" 4 2 "$d3" 4 20 40 40 \
-		2> /tmp/ui.dialog
-		if [ "$?" = "1" ]
-		then
-			continue
-		fi
-		disk_rw
-		echo -n "" > /etc/resolv.conf
-		s="yes"
-		for dns in `cat /tmp/ui.dialog`
-		do
-			if [ "$s" = "yes" ]
-			then
-				echo "search $dns" >>  /etc/resolv.conf
-				s="no"
-			else
-				echo "nameserver $dns" >> /etc/resolv.conf
-			fi
-		done
-		disk_ro
+		menu_dns
 	fi
 	if [ "`cat /tmp/ui.dialog`" = "p" ]
 	then
-		dialog --inputbox "Type an address to ping" \
-		7 40 www.tele2.se 2> /tmp/ui.dialog
-		ping -c 10 `cat /tmp/ui.dialog` &> /tmp/ui.dialog &
-		dialog --tailbox /tmp/ui.dialog 20 75
-		pkill -9 ping
+		menu_ping
+	fi
+	if [ "`cat /tmp/ui.dialog`" = "l" ]
+	then
+		menu_log
 	fi
 	if [ "`cat /tmp/ui.dialog`" = "s" ]
 	then
-		clear # clear screen
-		echo "Starting bash..."
-		reset # reset terminal; dialog may fuck it up :)
-		echo "Press CTRL+d to return to the console."
-		bash
+		menu_shell
 	fi
 done
 
